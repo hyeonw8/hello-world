23d0bce1c728d536a84f3dc17910bb425c0057ec
hyeonw8 <whgusrud223@gmail.com>
2023-04-10 22:36:48 +0900

    카드 생성하고, 뒤집기

--- Javascript/cardpair/index.js
+++ Javascript/cardpair/index.js
@@ -1,1 +1,50 @@
 const $wrapper = document.querySelector('#wrapper');
+
+const total = 12; // 처음 카드 12장 만들어서 배치
+const colors = ['red', 'orange', 'yellow', 'green', 'white', 'pink'];
+let colorCopy = colors.concat(colors); // concat 원본수정하지 않고 새로운 배열 생성,얕은 복사
+let shuffled = [];
+
+function shuffle() { // 피셔-예이츠 셔플
+  for (let i = 0; colorCopy.length > 0; i += 1) {
+    const randomIndex = Math.floor(Math.random() * colorCopy.length);
+    shuffled = shuffled.concat(colorCopy.splice(randomIndex, 1));
+  }
+}
+                          // > 자식 표시
+function createCard(i) { // div.card > div.card-inner > (div.card-front + div.card-back)
+  const card = document.createElement('div');
+  card.className = 'card'; // .card 태그 생성
+  const cardInner = document.createElement('div');
+  cardInner.className = 'card-inner'; // .card-inner 태그 생성
+  const cardFront = document.createElement('div');
+  cardFront.className = 'card-front'; // .card-front 태그 생성
+  const cardBack = document.createElement('div');
+  cardBack.className = 'card-back'; // .card-back 태그 생성
+  cardBack.style.backgroundColor = shuffled[i];
+  cardInner.appendChild(cardFront);
+  cardInner.appendChild(cardBack);
+  card.appendChild(cardInner);
+  return card;
+}
+
+function startGame() {
+  shuffle();
+  for (let i = 0; i < total; i += 1) {
+    const card = createCard(i);
+    $wrapper.appendChild(card);
+  }
+
+  document.querySelectorAll('.card').forEach((card, index) => { // 초반 카드 공개
+    setTimeout(() => {
+      card.classList.add('flipped');
+    }, 1000 + 100 * index);
+  });
+
+  setTimeout(() => { // 카드 감추기
+    document.querySelectorAll('.card').forEach((card) => {
+      card.classList.remove('flipped');
+    }); 
+  }, 5000);
+}
+startGame(); 
